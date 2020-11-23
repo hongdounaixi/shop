b56901e24db6232f098bf1deb5b92b1a3f9b347a
root <1635553432@qq.com>
2020-11-23 11:17:48 +0800

    修改了java文件的内容，增加了一个info方法

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,11 @@ public class HelloWorld {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
+		info();
+	}
+
+	public static void info()
+	{
+		System.out.println("这是一个info方法");
 	}
 }
